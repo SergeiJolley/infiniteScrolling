1d9552075ac790fa8f04861628fb5b0fabd377b4
unknown <jolley.sergei@gmail.com>
2020-07-24 12:39:17 -0600

    fixedLoader

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const postsContainer = document.getElementById('posts-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 
@@ -22,7 +22,7 @@ async function showPosts() {
         const postEl = document.createElement('div');
         postEl.classList.add('post');
         postEl.innerHTML = `
-        <div class ="number">${posts.id}</div>
+        <div class ="number">${posts.page}</div>
         <div class ="posts-info">
         <h2 class="posts-title">${posts.title}</h2>
         <p class="post-body">${post.body}</p>
@@ -33,17 +33,30 @@ async function showPosts() {
     });
 }
 
+function showLoading() {
+    loading.classList.add('show');
+
+    setTimeout(() => {
+        loading.classList.remove('show');
+
+        setTimeout(() => {
+            page++;
+            showPosts();
+        }, 300);
+    }, 1000);
+}
+
 showPosts();
 
-// window.addEventListener('scroll', () => {
-//     const {
-//         scrollTop,
-//         scrollHeight,
-//         clientHeight
-//     } =
-//     document.documentElement;
+window.addEventListener('scroll', () => {
+    const {
+        scrollTop,
+        scrollHeight,
+        clientHeight
+    } =
+    document.documentElement;
 
-//     if (scrollTop + clientHeight >= scrollHeight - 5) {
-
-//     }
-// });
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+        showLoading();
+    }
+});
